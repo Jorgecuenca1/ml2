a4f5807df08d71f0bb793b8b3f418b9018ed4ab0
Jorgecuenca1 <jorge.cuenca@unillanos.edu.co>
2023-04-24 18:36:01 +0000

    Kk

---
+++ model.py
@@ -0,0 +1,1 @@
+print("corro el modelo")
---
+++ principal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -r weather-dataset-rattle-package.zip 
+rm -r weatherAUS.csv 
+kaggle datasets download -d jsphyg/weather-dataset-rattle-package --force
+unzip weather-dataset-rattle-package.zip
+#psql -h 130.211.118.80 -d postgres -U postgres -c "\copy weather_1 FROM '~/weatherAUS.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',')" -W ml2password
+python3 model.py
+python3 connection.py
