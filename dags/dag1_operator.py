84407f304bdb919a52f8b3a4cb351e15d211304a
Jorgecuenca <jorge.cuenca@unillanos.edu.co>
2023-04-24 17:33:29 -0500

    kk

--- dags/dag1_operator.py
+++ dags/dag1_operator.py
@@ -13,3 +13,14 @@ class Dag1Operator(BaseOperator):
     @retry(5)
     def execute(self, context):
         print("ingreso")
+        import os
+
+        print("HOLA ACA INGRESO")
+
+        os.remove("weather-dataset-rattle-package.zip")
+        os.remove("weatherAUS.csv")
+        os.system("kaggle datasets download -d jsphyg/weather-dataset-rattle-package --force")
+        os.system("unzip weather-dataset-rattle-package.zip")
+        # os.system("psql -h 130.211.118.80 -d postgres -U postgres -c \"\copy weather_1 FROM '~/weatherAUS.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',')\" -W ml2password")
+        os.system("python3 model.py")
+        os.system("python3 connection.py")
