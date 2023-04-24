969a78282fe036fc5d205488a5019eb4b2a3d09c
Jorgecuenca <jorge.cuenca@unillanos.edu.co>
2023-04-24 16:26:32 -0500

    Kk

--- dags/dag1.py
+++ dags/dag1.py
@@ -1,50 +1,24 @@
+from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from datetime import datetime
-from airflow.operators.bash import BashOperator
-from dag1_operator import (
-    Dag1Operator
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 4, 24),
+    'retries': 0,
+}
+
+dag = DAG(
+    'my_dag',
+    default_args=default_args,
+    description='A DAG to run a bash script every 2 minutes',
+    schedule_interval=timedelta(minutes=2),
+    catchup=False
 )
 
-dag_id = "dag1"
-
-
-def dummy_callable(action):
-    return f"{datetime.now()}: {action} login and Dag1!"
-
-
-def create_dag(id):
-    with DAG(
-            dag_id=id,
-            description=f"Requests for Login and Dag1",
-            schedule_interval="*/10 * * * *",
-            start_date=datetime(2023, 4, 24),
-            catchup=False,
-            is_paused_upon_creation=False
-    ) as dag:
-        start = PythonOperator(
-            task_id="starting_pipeline",
-            python_callable=dummy_callable,
-            op_kwargs={"action": "starting"},
-            dag=dag
-        )
-        print(dag.folder)
-        t1 = BashOperator(
-            task_id='train_models',
-            bash_command='bash "../principal.sh"',
-            cwd=dag.folder
-        )
-
-        finish = PythonOperator(
-            task_id="finishing_pipeline",
-            python_callable=dummy_callable,
-            op_kwargs={"action": "finishing"},
-            dag=dag
-        )
-
-        start >> t1 >> finish
-
-    return dag
-
-
-globals()[dag_id] = create_dag(dag_id)
+run_script = BashOperator(
+    task_id='run_script',
+    bash_command='../principal.sh',
+    dag=dag
+)
